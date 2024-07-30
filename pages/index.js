c108f5409d45c9b3b413e40f925586da12481e8a
Pro-official <promise.official000@gmail.com>
2024-07-30 21:03:33 +0600

    remove console log

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export async function getStaticProps() {
 }
 
 export default function Home({ categories }) {
-  console.log(categories);
   return (
     <div className={styles.container}>
       <Head>
